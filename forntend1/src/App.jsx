9b147cce706f548142b5e691fe2d1d1132a0513c
parthiv kumar <dparthivkumar@gmail.com>
2026-09-11 00:46:17 +0530

    workin

--- forntend1/src/App.jsx
+++ forntend1/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
 function App(){
+  const  da= async()=>{
+ let a= await fetch(
+
+     "https://huggingface.co/api/datasets/TfqDeadlox636/icrm-hitek-fulldb/parquet/default/train");
+     let b = await a.json();
+     console.log(b)
+  }
+
   return(
     <div className='main'>
      <div className="mainco">
       <h1> LOGIN </h1>
      <input type="text" placeholder='enter the user name' />
      <input type="text"  placeholder='enter the password' />
+     <a href="">forgot password</a>
      <div className="in">
-      <button>sigin</button>
+      <button onClick={da}>sigin</button>
      <button>register</button>
      </div>
      </div>
